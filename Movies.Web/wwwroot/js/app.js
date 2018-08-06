bfc614f321026164cfeb7abc5604eb754f82594d
Edmund Smith <edmundsmith@MacBook-Pro.local>
2018-08-06 22:44:44 +1000

    Add Angular JS

---
+++ Movies.Web/wwwroot/js/app.js
@@ -0,0 +1,30 @@
+﻿// Define the `movies` module
+var phonecatApp = angular.module('movies', []);
+
+phonecatApp.controller('moviesListController', function($scope) {
+    $scope.refresh = function(){
+
+        $.ajax({
+            url:"/Main/GetMovies",
+            async: false,
+            success:function(data){
+            $scope.movieList = JSON.parse(data);
+            }
+
+      });
+
+    };
+
+    $scope.refresh();
+
+ /*  $.ajax({
+            url:"/Main/GetMovies",
+            async: false,
+            success:function(data){
+            $scope.movieList = JSON.parse(data);
+            }
+
+      });*/
+     
+  
+});
