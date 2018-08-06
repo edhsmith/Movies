2b62f1ce30361cf6ded5756b21ed0c01b4360823
Edmund Smith <edmundsmith@MacBook-Pro.local>
2018-08-06 21:02:17 +1000

    Create project file.

---
+++ Movies.Web/Controllers/MainController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Movies.Web.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetMovies(){
+
+            WebRequest req = WebRequest.CreateHttp("http://webjetapitest.azurewebsites.net/api/cinemaworld/movies");
+            req.Headers.Add("x-access-token", "sjd1HfkjU83ksdsm3802k");
+            string result = string.Empty;
+
+            using(HttpWebResponse resp = (HttpWebResponse)req.GetResponse()){
+                using(StreamReader sr = new StreamReader(resp.GetResponseStream())){
+                    result = sr.ReadToEnd();
+                    sr.Close();
+                }
+                resp.Close();
+            }
+            return this.Json(result);
+        }
+    }
+}
