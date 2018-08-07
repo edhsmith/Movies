73503179e72a6d842e6fc6e73aa2394a2ba8ca1a
Edmund Smith <smithed@ROADS.VIC.GOV.AU>
2018-08-08 09:35:15 +1000

    Show detail now loads automatically.

--- Movies.Mvc/Scripts/app.js
+++ Movies.Mvc/Scripts/app.js
@@ -2,7 +2,7 @@
 var thisApp = angular.module('movies', []);
 
 thisApp.controller('moviesListController', function ($scope) {
-
+    
     $scope.refresh = function () {
 
         $scope.movieList = { Movies:[] };
@@ -19,6 +19,30 @@ thisApp.controller('moviesListController', function ($scope) {
       });
 
     };
+
+    $scope.getDetail = function (id, idx) {
+
+        $.ajax({
+            url: "/Main/GetMovieDetail/" + id,
+            async: true,
+            success: function (data) {
+                $scope.movieDetail = JSON.parse(data);
+                var movieDetail = JSON.parse(data);
+                $("#rated" + idx).html(movieDetail.Rated);
+                $("#rating" + idx).html(movieDetail.Rating);
+                $("#released" + idx).html(movieDetail.Released);
+                $("#runtime" + idx).html(movieDetail.Runtime);
+                $("#actors" + idx).html(movieDetail.Actors);
+                $("#price" + idx).html(movieDetail.Price);
+                $("div[controlId=" + idx + "]").removeClass("hidden");
+            },
+            error: function (jqXHR, textStatus, errorThrown) {               
+                alert("get detail error");
+            }
+        });
+
+
+    }
 
     $scope.showDetail = function (id) {
        // $scope.movieDetail = "";
